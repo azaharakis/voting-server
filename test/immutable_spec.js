8e115cad781ca91c1f494fbc13da06000f53c30a
Alexander Zaharakis <alex.zaharakis@rea-group.com>
2015-12-07 13:22:39 +1100

    Add tests around immutable lists

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,4 +1,5 @@
-import { expect ] from 'chai';
+import { expect } from 'chai';
+import { List, Map } from 'immutable';
 
 describe('immutability', () => {
 
@@ -10,8 +11,61 @@ describe('immutability', () => {
            let state = 42;
            let newState = increment(state);
 
-           expect(nextState).to.equal(43);
+           expect(newState).to.equal(43);
            expect(state).to.equal(42);
         });
+    });
+
+    describe('A list', () => {
+
+        const addMovie = (currentState, movie) => currentState.push(movie);
+
+        it('is immutable', () => {
+            let state = List.of('Trainspotting', '28 Days Later');
+            let nextState = addMovie(state, 'Sunshine');
+
+            expect(nextState).to.equal(List.of(
+                'Trainspotting',
+                '28 Days Later',
+                'Sunshine'
+            ));
+            expect(state).to.equal(List.of(
+                'Trainspotting',
+                '28 Days Later'
+            ));
+        });
+    });
+
+    describe('a tree', () => {
+
+        const addMovie = (currentState, movie) => {
+            return currentState.set(
+                'movies',
+                currentState.get('movies').push(movie)
+            );
+        }
+
+        it('is immutable', () => {
+            let state = Map({
+                movies: List.of('Trainspotting', '28 Days Later')
+            });
+
+            let nextState = addMovie( state, 'Sunshine');
+
+            expect(nextState).to.equal(Map({
+                movies: List.of(
+                    'Trainspotting',
+                    '28 Days Later',
+                    'Sunshine'
+                )
+            }));
+            expect(state).to.equal(Map({
+                movies: List.of(
+                    'Trainspotting',
+                    '28 Days Later'
+                )
+            }));
+        });
+
     });
 });
